52cf2c82587203aa24428a383c74fc95046325a8
afnan <afnanislam130@gmail.com>
2023-08-02 11:10:28 -0400

    final project

--- app/src/main/java/com/example/work/HighScoreActivity.java
+++ app/src/main/java/com/example/work/HighScoreActivity.java
@@ -5,6 +5,9 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,7 +16,13 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HighScoreActivity extends AppCompatActivity {
+/**
+ *
+ * This class creates save the name of users in the database, also set on click listener for button
+ * to show the name of high scorers
+ * @author afnan
+ */
+public class HighScoreActivity extends AppCompatActivity implements HighScoreAdapter.HighScoreItemClickListener {
 
     private RecyclerView recyclerView;
     private HighScoreAdapter adapter;
@@ -21,6 +30,14 @@ public class HighScoreActivity extends AppCompatActivity {
 
     private AppDatabase appDatabase; // Room database instance
 
+
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +57,51 @@ public class HighScoreActivity extends AppCompatActivity {
         highScoresList = new ArrayList<>();
 
         // Create an instance of the HighScoreAdapter and set it to the RecyclerView
-        adapter = new HighScoreAdapter(highScoresList);
+        adapter = new HighScoreAdapter(highScoresList, this);
         recyclerView.setAdapter(adapter);
 
         // Load and display the high scores from the Room database
         loadHighScoresFromDatabase();
     }
+
+
+    /**
+     * shows the scorer's name by clicking the button
+     * @param highScorerName  name of users who scored highest
+     */
+    @Override
+    public void onHighScoreItemClick(String highScorerName) {
+        // Show the HighScorerNameFragment when a high score is clicked
+        showHighScorerNameFragment(highScorerName);
+    }
+
+    /**
+     *
+     * @param highScorerName a fragment manager for showing the name of users
+     *                       who has ranked from highest to lowest
+     */
+    private void showHighScorerNameFragment(String highScorerName) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Find the existing fragment by tag and remove it if it exists
+        Fragment existingFragment = fragmentManager.findFragmentByTag("HighScorerNameFragment");
+        if (existingFragment != null) {
+            fragmentTransaction.remove(existingFragment);
+        }
+
+        HighScorerNameFragment fragment = HighScorerNameFragment.newInstance(highScorerName);
+
+        // Replace the fragment container with the HighScorerNameFragment
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment, "HighScorerNameFragment");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
+
+
+
 
     // Method to display the top 10 high scores from the database
     private void showTopHighScores(List<HighScore> topHighScores) {
